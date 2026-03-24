c2ac8233ae3b94eebc84ca8c5974b2a114ac6d51
lambda-0001 <a3144098136@gmail.com>
2026-03-24 09:03:55 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,30 @@
+import albumentations as A
+from ultralytics import YOLO
+
+
+# Load a model
+model = YOLO("./weights/yolov8n.pt")  
+
+if __name__ == '__main__':
+    
+    train_results = model.train(
+        data="./ultralytics/cfg/datasets/train2025.yaml",
+        batch=1,
+        imgsz=120,
+        device='cpu',
+        optimizer='SGD',
+        epochs=100,
+        hsv_h=0.3,
+        hsv_s=0.7,
+        hsv_v=0.4,
+        translate=0.5,
+        scale=0.5,
+        fliplr=0.5,
+        mosaic=0.5,
+        erasing=0.5,
+        auto_augment=None,
+    )
+
+
+
+
